2a0db3e91956c7794f32ce4031592c02093aa37d
c-nakashima <chisatonakashima1025@gmail.com>
2026-06-15 10:33:17 +1000

    Add initial seed data

---
+++ backend/sql/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO tasksets (title, description)
+VALUES
+('Basic Day Tasks', 'Usual tasks before leaving home'),
+('Sleepover Tasks', 'Tasks before staying overnight'),
+('Gig Tasks', 'Things to bring before going to a gig');
