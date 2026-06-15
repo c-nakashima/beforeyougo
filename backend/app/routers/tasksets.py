e590f646b4778afdda68aadfafcee96e5a010ef0
c-nakashima <chisatonakashima1025@gmail.com>
2026-06-15 22:25:36 +1000

    Add: task creation API

--- backend/app/routers/tasksets.py
+++ backend/app/routers/tasksets.py
@@ -16,10 +16,21 @@ class TasksetCreate(BaseModel):
     max_length=250,
   )
 
+class TaskCreate(BaseModel):
+  title: str = Field(
+    min_length=1,
+    max_length=100,
+  )
+  sort_order:int | None = Field(
+    default=None,
+    ge=0,
+  )
+
 router = APIRouter(
   prefix="/tasksets",
   tags=["tasksets"],
 )
+
 
 @router.get("")
 def get_tasksets():
@@ -53,7 +64,7 @@ def get_tasksets():
 
 
 @router.get("/{taskset_id}")
-def get_tasksets(taskset_id: UUID):
+def get_taskset(taskset_id: UUID):
   taskset_query = """
     SELECT
       id,
@@ -92,7 +103,8 @@ def get_tasksets(taskset_id: UUID):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Taskset not found",
           )
-
+        
+        # retreive tasks belong to the taskset
         cursor.execute(
           tasks_query,
           (str(taskset_id,),)
@@ -112,9 +124,6 @@ def get_tasksets(taskset_id: UUID):
           status_code=500,
           detail="Failed to retrieve taskset",
       ) from error
-
-
-
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
@@ -146,4 +155,68 @@ def create_taskset(taskset: TasksetCreate):
     raise HTTPException(
       status_code=500,
       detail="Failed to create taskset",
+    ) from error
+
+
+@router.post("/{taskset_id}/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(
+  taskset_id: UUID,
+  task: TaskCreate
+):
+  check_taskset_query = """
+      SELECT id
+      FROM tasksets
+      WHERE id = %s;
+  """
+
+  insert_task_query = """
+    INSERT INTO tasks (
+      taskset_id,
+      title,
+      sort_order
+    )
+    VALUES (%s, %s, %s)
+    RETURNING
+      id,
+      taskset_id,
+      title,
+      sort_order,
+      created_at
+      updated_at;
+  """
+
+  try:
+    with get_connection() as connection:
+      with connection.cursor() as cursor:
+        # Check if taskset exists
+        cursor.execute(
+          check_taskset_query,
+          (str(taskset_id),)
+        )
+        existing_taskset = cursor.fetchone()
+
+        # If existing_taskset is None
+        if existing_taskset is None:
+          raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Taskset not found"
+          )
+        
+        # If existing_taskset exists, add task
+        cursor.execute(
+          insert_task_query,
+          (
+            str(taskset_id),
+            task.title,
+            task.sort_order,
+          )
+        )
+        created_task = cursor.fetchone()
+
+    return created_task
+
+  except Error as error:
+    raise HTTPException(
+      status_code=500,
+      detail="Failed to create task",
     ) from error
