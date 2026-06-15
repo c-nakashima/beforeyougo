7287b3ffb7344a6240b713a952814a6a5855173d
c-nakashima <chisatonakashima1025@gmail.com>
2026-06-15 12:07:32 +1000

    Add: db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -5,18 +5,17 @@ import psycopg2
 from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
 
-
-ENV_PATH = Path(__file__).resolve().parents[1] / ".env"
+# Get dotenv file path
+ENV_Path = Path(__file__).resolve().parents[1] / ".env"
+# Load dotenv file and store in os as a environment variable
 load_dotenv(ENV_PATH)
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+# If DATABASE_URL is not set, raise an error
 if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL is not set in backend/.env")
-
+  raise RuntimeError("DATABASE_URL is not set in backend/.env")
 
 def get_connection():
-    return psycopg2.connect(
-        DATABASE_URL,
-        cursor_factory=RealDictCursor,
-    )
+  return psycopg2.connect(
+    DATABASE_URL,
+    cursor_factory=RealDictCursor,
+  )
